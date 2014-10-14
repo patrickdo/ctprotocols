c96114051358a539d8416865c08b2ca698a7d537
Patrick Do <patrickdo@gmail.com>
2014-10-13 22:08:26 -0700

    first try at csv

--- script.js
+++ script.js
@@ -27,62 +27,14 @@ protocolList.add({
 
 protocolList.add(values);
 
-var xmlDoc = loadXMLDoc('test.xml');
-// alert(xmlDoc.documentElement.nodeName);
+var CSVProtocols = new XMLHttpRequest();
+CSVProtocols.open('GET', 'ctprotocols.csv');
+CSVProtocols.onreadystatechange = function() {
+	alert(CSVProtocols.responseText);
+};
+CSVProtocols.send();
 
 
 
 
 
-function loadXMLDoc(XMLname) {
-	var xmlDoc;
-
-	if (window.XMLHttpRequest) {
-		xmlDoc = new window.XMLHttpRequest();
-		xmlDoc.open("GET", XMLname, false);
-		xmlDoc.overrideMimeType('text/xml');
-		xmlDoc.send("");
-		return xmlDoc.responseXML;
-	}
-
-	// IE 5 and IE 6
-	else if (ActiveXObject("Microsoft.XMLDOM")) {
-		xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-		xmlDoc.async = false;
-		xmlDoc.load(XMLname);
-		return xmlDoc;
-	}
-	else {
-		xmlhttp = new XMLHttpRequest();
-
-		//Open the file using the GET routine
-		xmlhttp.open("GET", XMLname, false);
-
-		xmlhttp.overrideMimeType('text/xml');
-
-		//Send request
-		xmlhttp.send(null);
-
-		//xmlDoc holds the document information now
-		return xmlDoc.responseXML;
-	}
-
-	return null;
-}
-
-
-
-
-
-
-
-
-
-
-// XML STUFF THAT DOESN'T WORK
-// xmlhttp = new XMLHttpRequest();
-// xmlhttp.open('GET', 'test.xml', false);
-// xmlhttp.send();
-// xmlDoc = xmlhttp.responseXML;
-
-// console.log(xmlDoc.getElementsByTagName('from')[0].childNodes[0].nodeValue);
