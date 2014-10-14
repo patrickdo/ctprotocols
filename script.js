4f10cf75c85870a4d25a9902bce4807b292742a2
Patrick Do <patrickdo@gmail.com>
2014-10-14 00:14:46 -0700

    revert

--- script.js
+++ script.js
@@ -2,46 +2,45 @@
 // var options = {
 // 	valueNames: ['protNum', 'protDesc', 'protIndic', 'protMDCT']
 // };
-//
+
 // var protocolList = new List('protocolDIV', options);
+
+// STUFF THAT WORKS
+
+var options = {
+	valueNames: ['protNum', 'protDesc', 'protIndic', 'protMDCT']
+};
+
 // var values = [
 // 	{protNum: '285', protDesc: 'Stockholm', protIndic: 'indication', protMDCT: 'MDCT protocol'}
 // ];
+
+var protocolList = new List('protocolDIV', options);
+protocolList.remove({protNum: '0'});
+
 // protocolList.add({
 // 	protNum: '284',
 // 	protDesc: 'descrip',
 // 	protIndic: 'indication',
 // 	protMDCT: 'mdct protocol'
 // });
+
 // protocolList.add(values);
 
-var options = {
-	valueNames: ['protNum', 'protDesc', 'protIndic', 'protMDCT']
-};
-
-var protocolList = new List('protocolDIV', options);
-protocolList.remove({protNum: '0'});
-
 // CSV STUFF
+
 var CSVProtocols = new XMLHttpRequest();
-CSVProtocols.open('GET', 'ctprotocols.csv', true);
+CSVProtocols.open('GET', 'ctprotocols.csv');
 CSVProtocols.send();
 var CSVText = '', CSVLines = [];
 
-CSVProtocols.onreadystatechange = onCSVload();
-	// if (CSVProtocols.readyState == 4) {	// have to wait for AJAX call to complete
-	// 	CSVLines = CSVProtocols.responseText.split('\n');
-	// 	parseCSV();
-	// 	addProtocols();
-	// }
-
-function onCSVload() {
+CSVProtocols.onreadystatechange = function() {
 	if (CSVProtocols.readyState == 4) {	// have to wait for AJAX call to complete
 		CSVLines = CSVProtocols.responseText.split('\n');
 		parseCSV();
 		addProtocols();
 	}
-}
+};
 
 function addProtocols() {
 	var i, len;
@@ -65,9 +64,9 @@ function parseCSV() {
 function CSVtoArray(text) {
 	var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
 	var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
-	// return NULL if input string is not a well-formed CSV string.
+	// Return NULL if input string is not well formed CSV string.
 	if (!re_valid.test(text)) return null;
-	var a = [];
+	var a = [];	// Initialize array to receive values.
 	text.replace(re_value,	// "Walk" the string using replace with callback.
 		function(m0, m1, m2, m3) {
 			// Remove backslash from \' in single quoted values.
