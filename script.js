c52511e5a44b66ef5f8c2af5bd3a134e449a88a7
Patrick Do <patrickdo@gmail.com>
2014-10-13 23:09:09 -0700

    clean

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ var options = {
 	valueNames: ['protNum', 'protDesc', 'protIndic', 'protMDCT']
 };
 
-var values = [
-	{protNum: '285', protDesc: 'Stockholm', protIndic: 'indication', protMDCT: 'MDCT protocol'}
-];
+// var values = [
+// 	{protNum: '285', protDesc: 'Stockholm', protIndic: 'indication', protMDCT: 'MDCT protocol'}
+// ];
 
 var protocolList = new List('protocolDIV', options);
 protocolList.remove({protNum: '0'});
 
-protocolList.add({
-	protNum: '284',
-	protDesc: 'descrip',
-	protIndic: 'indication',
-	protMDCT: 'mdct protocol'
-});
+// protocolList.add({
+// 	protNum: '284',
+// 	protDesc: 'descrip',
+// 	protIndic: 'indication',
+// 	protMDCT: 'mdct protocol'
+// });
 
 protocolList.add(values);
 
@@ -31,9 +31,6 @@ protocolList.add(values);
 
 var CSVProtocols = new XMLHttpRequest();
 CSVProtocols.open('GET', 'ctprotocols.csv');
-// CSVProtocols.onreadystatechange = function() {
-// 	alert(CSVProtocols.responseText);
-// };
 CSVProtocols.send();
 var CSVText = '', CSVLines = [];
 
@@ -44,7 +41,6 @@ CSVProtocols.onreadystatechange = function() {
 		addProtocols();
 	}
 };
-
 
 function addProtocols() {
 	var i, len;
@@ -58,7 +54,6 @@ function addProtocols() {
 	}
 }
 
-// CSVLines = CSVProtocols.responseText.split('\n');
 function parseCSV() {
 	var i, len;
 	for (i = 0, len = CSVLines.length; i < len; i++) {
