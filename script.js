e764c1ef0925ca53d6080c97c145bc4fddce1d7e
Patrick Do <patrickdo@gmail.com>
2014-10-14 18:58:59 -0700

    add curly brakcets

--- script.js
+++ script.js
@@ -69,11 +69,11 @@ function CSVtoArray(text) {
 	text.replace(re_value,	// "Walk" the string using replace with callback.
 		function(m0, m1, m2, m3) {
 			// Remove backslash from \' in single quoted values.
-			if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+			if (m1 !== undefined) {a.push(m1.replace(/\\'/g, "'"));}
 			// Remove backslash from \" in double quoted values.
-			else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
-			else if (m3 !== undefined) a.push(m3);
-			return '';	// Return empty string.
+			else if (m2 !== undefined) {a.push(m2.replace(/\\"/g, '"'));}
+			else if (m3 !== undefined) {a.push(m3);}
+			return '';	// Return empty string. ???
 		});
 	// Handle special case of empty last value.
 	if (/,\s*$/.test(text)) a.push('');
