d880b68ae06e72beb6835b670aff477836c90dde
Patrick Do <patrickdo@gmail.com>
2014-10-15 23:08:55 -0700

    clean

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
-// ORIGINAL
-// var options = {
-// 	valueNames: ['protNum', 'protDesc', 'protIndic', 'protMDCT']
-// };
-
-// var protocolList = new List('protocolDIV', options);
-
-// STUFF THAT WORKS
-
 var options = {
 	valueNames: ['protNum', 'protDesc', 'protIndic', 'protMDCT'],
 	page: [2000]
 };
 
-// var values = [
-// 	{protNum: '285', protDesc: 'Stockholm', protIndic: 'indication', protMDCT: 'MDCT protocol'}
-// ];
-
 var protocolList = new List('protocolDIV', options);
 protocolList.remove({protNum: '0'});
 protocolList.remove({protNum: ''});
 
-// protocolList.add({
-// 	protNum: '284',
-// 	protDesc: 'descrip',
-// 	protIndic: 'indication',
-// 	protMDCT: 'mdct protocol'
-// });
-
-// protocolList.add(values);
-
 // CSV STUFF
-
 var CSVProtocols = new XMLHttpRequest(), CSVText = '', CSVLines = [];
 CSVProtocols.open('GET', 'ctprotocols.csv', true);
 CSVProtocols.overrideMimeType("text/plain");
@@ -44,6 +21,13 @@ CSVProtocols.onreadystatechange = function() {
 	}
 };
 
+function parseCSV() {
+	var i, len;
+	for (i = 1, len = CSVLines.length; i < len; i++) {
+		CSVLines[i] = CSVtoArray(CSVLines[i]);
+	}
+}
+
 function addProtocols() {
 	var i, len;
 	for (i = 1, len = CSVLines.length; i < len; i++) {
@@ -56,35 +40,7 @@ function addProtocols() {
 	}
 }
 
-function parseCSV() {
-	var i, len;
-	for (i = 1, len = CSVLines.length; i < len; i++) {
-		CSVLines[i] = csv2array(CSVLines[i]);
-	}
-}
-
-function CSVtoArray(text) {
-	var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
-	var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
-	// Return NULL if input string is not well formed CSV string.
-	if (!re_valid.test(text)) return null;
-	var a = [];	// Initialize array to receive values.
-	text.replace(re_value,	// "Walk" the string using replace with callback.
-		function(m0, m1, m2, m3) {
-			// Remove backslash from \' in single quoted values.
-			if (m1 !== undefined) {a.push(m1.replace(/\\'/g, "'"));}
-			// Remove backslash from \" in double quoted values.
-			else if (m2 !== undefined) {a.push(m2.replace(/\\"/g, '"'));}
-			else if (m3 !== undefined) {a.push(m3);}
-			// else
-			// {return '';}	// Return empty string. ???
-		});
-	// Handle special case of empty last value.
-	if (/,\s*$/.test(text)) {a.push('');}
-	return a;
-}
-
-function csv2array(data, delimiter) {
+function CSVtoArray(data, delimiter) {
 	// Retrieve the delimiter
 	if (delimiter === undefined) {
 		delimiter = ',';
