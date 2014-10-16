a424f4f2268d67778bde83846b3b1faaa1104db9
Patrick Do <patrickdo@gmail.com>
2014-10-15 21:59:46 -0700

    get rid of a.push

--- script.js
+++ script.js
@@ -72,14 +72,17 @@ function CSVtoArray(text) {
 	text.replace(re_value,	// "Walk" the string using replace with callback.
 		function(m0, m1, m2, m3) {
 			// Remove backslash from \' in single quoted values.
-			if (m1 !== undefined) {a.push(m1.replace(/\\'/g, "'"));}
+			if (m1 !== undefined) {a[a.length] = m1.replace(/\\'/g, "'");}
+			// if (m1 !== undefined) {a.push(m1.replace(/\\'/g, "'"));}
 			// Remove backslash from \" in double quoted values.
-			else if (m2 !== undefined) {a.push(m2.replace(/\\"/g, '"'));}
-			else if (m3 !== undefined) {a.push(m3);}
-			// else
-			// {return '';}	// Return empty string. ???
+			else if (m2 !== undefined) {a[a.length] = m2.replace(/\\"/g, '"');}
+			// else if (m2 !== undefined) {a.push(m2.replace(/\\"/g, '"'));}
+			else if (m3 !== undefined) {a[a.length] = m3;}
+			// else if (m3 !== undefined) {a.push(m3);}
+			else
+			{return '';}	// Return empty string. ???
 		});
 	// Handle special case of empty last value.
-	if (/,\s*$/.test(text)) {a.push('');}
+	if (/,\s*$/.test(text)) {a[a.length] = '';}
 	return a;
 }
